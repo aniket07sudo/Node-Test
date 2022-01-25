b5a841082364b36f698d62aee1beb8d6ac564aa7
aniket.sharma1 <Aniket@ANIKET.Aniket>
2022-01-26 02:36:45 +0530

    First Commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/", (req,res) => {
     res.send("Node Js Server Running")
 })
 
-app.listen(process.env.PORT || 4000, function () {
+app.listen(4000, function () {
     
     console.log("Node Application Running");
 })
